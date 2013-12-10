5ef33c55dd9d6398c28afda3a8842edcfda2b9bd
m1kael <1@1.com>
2013-12-10 11:46:21 +1100

    1st object

---
+++ main.m
@@ -0,0 +1,52 @@
+#import <Foundation/Foundation.h>
+
+@interface Fraction : NSObject
+- (void) setNumerator: (int) n;
+- (void) setDenominator: (int) d;
+- (void) print;
+@end
+
+@implementation Fraction
+int numerator;
+int denominator;
+- (void) print
+{
+	NSLog(@"%i/%i", numerator, denominator);
+}
+- (void) setNumerator: (int) n
+{
+	numerator = n;
+}
+- (void) setDenominator: (int) d
+{
+	denominator = d;
+}
+
+@end
+ 
+int main (int argc, const char *argv[]) {
+
+	/*
+	@autoreleasepool {
+		int sum = 25 - 37 + 19;
+		NSLog(@'The result is %i', sum);
+	}
+	*/
+ 
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+    NSLog(@"***Sweet tutos***\n");
+    NSLog(@"***Hello World!***\n");//This will output Hello World!
+	int sum = 25 - 37 + 19;
+	NSLog(@"The result is %i", sum);
+	
+	Fraction *fraction = [Fraction alloc];
+	fraction = [fraction init];
+	[fraction setNumerator: 1];
+	[fraction setDenominator: 3];
+	NSLog(@"Fraction is: ");
+	[fraction print];
+    
+	[pool release];
+    return 0;
+}
